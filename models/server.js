d03c30d409c11afc816046c905fdd6b676d1b636
Edgar Ramirez <edgramir17@hotmail.com>
2023-01-19 09:57:10 -0600

    Default port updated

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
         //Middlewares (funcion que se ejecuta cuando levantamos el servidor)
         this.middlewares();
