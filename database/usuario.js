54eecaace333bf84a1eaa3fd4e307c9000207e93
Edgar Ramirez <edgramir17@hotmail.com>
2023-01-20 00:47:49 -0600

    Funcion para configurar la cadena de conexion a mongo

---
+++ database/usuario.js
@@ -0,0 +1,36 @@
+
+const { Schema, model } = require( 'mongoose' ); 
+
+const usuarioSchema = Schema({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+}
+)
