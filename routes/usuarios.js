4d408ffd2d3bb1054684e5a2392b7666cfe1cd61
Edgar Ramirez <edgramir17@hotmail.com>
2023-01-21 17:21:58 -0600

    Se agregaron validaciones de campos del modelo previos al registro en bd

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,10 @@
 
 
 const { Router } = require( 'express' );
+const { check } = require( 'express-validator' );
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRolValido, emailExiste } = require('../helpers/db-validators');
+
 const { usuariosGet,
         usuariosPost,
         usuariosPatch,
@@ -14,7 +18,15 @@ router.get('/', usuariosGet);
 
 router.put( '/:id', usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/', [ 
+     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+     check('password', 'El password debe de ser de más de 6 letras').isLength({ min: 6 }),
+     check( 'correo', 'El correo no es válido' ).isEmail(),
+     check( 'correo').custom( emailExiste ),
+     //check( 'rol', 'No es un rol válido' ).isIn(['USER_ROLE', 'ADMIN_ROLE']),
+     check( 'rol' ).custom( esRolValido ),
+     validarCampos
+], usuariosPost);
 
 router.delete('/', usuariosDelete);
 
