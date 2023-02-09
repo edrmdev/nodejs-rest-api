71425164e0bf72544711dfa858c9971a630390c9
Edgar Ramirez <edgramir17@hotmail.com>
2023-02-09 17:20:42 -0600

    Se agrego middleware para validar que exista archivo antes de realizar peticion de subida a index

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,12 @@
 //Middlewares
 
 const validaCampos = require('../middlewares/validar-campos');
+const validarSubirArchivo = require( '../middlewares/validar-archivo');
 const validarJWT  = require( '../middlewares/validar-jwt' );
 const validaRoles = require('../middlewares/validar-roles');
 
 module.exports = {
+    ...validarSubirArchivo,
     ...validaCampos,
     ...validarJWT,
     ...validaRoles,
