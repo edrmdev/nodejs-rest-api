f227875474b3ef469e38e410f336008744773b52
Edgar Ramirez <edgramir17@hotmail.com>
2023-02-09 17:17:28 -0600

    Helper para validacion de colecciones permitidas (ruta busqueda)

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -51,7 +51,20 @@ const existeProductoPorID = async ( id = '' ) => {
     }
 }
 
+//Validar colecciones permitidas
+const coleccionesPermitidas = ( coleccion = '', colecciones = []) => {
+    
+    const incluida = colecciones.includes( coleccion );
+
+    if( !incluida ) {
+        throw new Error(`La coleccion ${ coleccion } no es permitida, ${ colecciones }`);
+    }
+
+    return true;
+}
+
 module.exports = {
+    coleccionesPermitidas,
     emailExiste,
     esRolValido,
     existeCategoriaPorID,
